2eda5c4547fab0bcd0bf615e25c83de198dfc953
Vikram <jonn.pomor@gmail.com>
2021-04-12 02:06:05 +0200

    Зеленый гитХаб - миф?

---
+++ Вёрстка_целых_страниц/СайтПортфоли(ИЗГОЛОВЫ)/app.js
@@ -0,0 +1,54 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.getElementById("form");
+  form.addEventListener("submit", formSend);
+
+  async function formSend(e) {
+    e.preventDefault();
+    let error = formValidate(form);
+
+    if (error === 0) {
+    } else {
+      alert("Заполните обязательные поля!");
+    }
+  }
+
+  function formValidate(form) {
+    let error = 0;
+    let formReq = document.querySelectorAll("._req");
+
+    for (let index = 0; index < formReq.length; index++) {
+      const input = formReq[index];
+      formRemoveError(input);
+
+      if (input.classList.contains("_email")) {
+        if (!validateEmail(input)) {
+          formAddError(input);
+          error++;
+        } else {
+          formRemoveError(input);
+        }
+      } else {
+        if (input.value === "") {
+          formAddError(input);
+          error++;
+        }
+      }
+    }
+    return error;
+  }
+
+  function formAddError(input) {
+    input.parentElement.classList.add("_error");
+    input.classList.add("_error");
+  }
+  function formRemoveError(input) {
+    input.parentElement.classList.remove("_error");
+    input.classList.remove("_error");
+  }
+  function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+  }
+  console.log(validateEmail("asgsgdgsd"));
+  console.log(validateEmail("jonn"));
+});
---
+++ Мои_проекты/SpringForce/app.js
@@ -0,0 +1,14 @@
+let y = 250;
+let restLength = 200;
+function setup() {
+  createCanvas(600, 400);
+}
+
+function draw() {
+  background(112, 50, 126);
+  noStroke();
+  fill(45, 197, 244);
+  circle(300, y, 64);
+
+  let x = y - restLength;
+}
